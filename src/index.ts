f58ca69943577450dde9d1f93dd3d15014342d37
Jéssica Lima <jessicalimabitt@gmail.com>
2023-03-17 11:37:32 -0300

    atualicacao

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import express from "express"
-import cors from 'cors'
 import { userRouter } from "./routes/userRouter";
 import { bandRouter } from "./routes/bandRouter";
 import { showRouter } from "./routes/showRouter";
+import { app } from "./app";
 
-const app = express()
-
-app.use(express.json())
 app.use("/users", userRouter);
 app.use("/band", bandRouter);
 app.use("/show", showRouter);
 
-app.use(cors())
-
-app.listen(3003, () => {
-    console.log("Server is running in http://localhost:3003");
-});
